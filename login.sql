d6e44d5c39435142eb3b476fdf397e27432e4f9c
Don Seiler <don@seiler.us>
2011-06-29 10:22:35 -0500

    Initial commit

---
+++ login.sql
@@ -0,0 +1,34 @@
+-- login.sql
+-- Don Seiler, don@seiler.us
+
+-- Convert tabs to spaces
+set tab off
+
+-- Do not show variable substition
+set verify off
+
+-- Do not truncate long
+set long 999999 longc 999999
+
+-- Display full numbers up to 15 chars
+set numwidth 15
+
+--
+-- Editing
+--
+def _editor=vi
+set editf /tmp/tmpsqlplus.sql
+
+--
+-- Spooling
+--
+
+-- Echo SQL statements to spool file
+set echo on
+
+-- Print elapsed time for each statement
+set timing on
+
+-- Trim trailing whitespace when spooling
+set trimspool off
+
