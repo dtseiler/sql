daa8a57febb5eba67fa48246aa98c41a52774919
Don Seiler <don@seiler.us>
2011-06-30 15:53:58 -0500

    - Made editfile name a bit more unique with yyyymmddhh24miss

--- login.sql
+++ login.sql
@@ -3,9 +3,6 @@
 
 -- To use, place login.sql in directory specified by SQLPATH env var.
 
--- Use this date format by default
-alter session set nls_date_format='yyyy/mm/dd hh24:mi:ss';
-
 -- Better safe than sorry
 set autocommit off
 
@@ -34,10 +31,19 @@ set time on
 -- Use vi as editor (must be in PATH)
 define _editor=vi
 
+-- Quiet while we're working
+set feedback off
+
 -- Use /tmp as location for edit file
--- XXX Increases chances of collisions with multiple users
---     * can we randomize file name with something like PID?
-set editfile /tmp/tmpsqlplus.sql
+-- XXX Security risk, can we put this into eg $SQLPATH/.tmp/ ?
+alter session set nls_date_format='yyyymmddhh24miss';
+set editfile /tmp/tmpsp.&&_date
+
+-- Use this date format by default
+alter session set nls_date_format='yyyy/mm/dd hh24:mi:ss';
+
+-- Noisy part is done
+set feedback on
 
 --
 -- Spooling
