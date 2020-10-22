81ca659f34a213ed0ad85644855e07c59d0fb530
Don Seiler <don@seiler.us>
2020-10-22 10:27:37 -0500

    Changing users to orders

--- postgresql/gen_data.sql
+++ postgresql/gen_data.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-  --id    SERIAL PRIMARY KEY,
-  id    INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR(40) NOT NULL UNIQUE,
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(
+  --order_id    SERIAL PRIMARY KEY,
+  order_id      INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email         VARCHAR(40) NOT NULL,
   created_at    TIMESTAMP
 );
 
-INSERT INTO users(email, created_at)
+INSERT INTO orders(email, created_at)
 SELECT
-  'user_' || seq || '@' || (
+  'order_' || seq || '@' || (
     CASE (RANDOM() * 2)::INT
       WHEN 0 THEN 'gmail'
       WHEN 1 THEN 'hotmail'
@@ -16,13 +16,13 @@ SELECT
     END
   ) || '.com' AS email,
   timestamp '2016-01-10 20:00:00' +
-       random() * (timestamp '2020-01-20 20:00:00' -
-                   timestamp '2016-01-10 20:00:00')
+       random() * (now() - timestamp '2016-01-10 20:00:00')
 FROM GENERATE_SERIES(1, 200000) seq;
 
-CREATE INDEX users_created_at_idx ON users(created_at);
+CREATE INDEX orders_email_idx ON orders (email);
+CREATE INDEX orders_created_at_idx ON orders (created_at);
 
 SELECT date_trunc('month', created_at) as created_month, count(*)
-FROM users
+FROM orders
 GROUP BY created_month
 ORDER BY created_month;
