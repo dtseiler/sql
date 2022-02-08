794ced0aed78b42d168d359e66024edc966f6e99
Don Seiler <don@seiler.us>
2022-02-08 16:36:54 -0600

    Look up default ACLs

---
+++ postgresql/default_acl.sql
@@ -0,0 +1,11 @@
+select 
+	pg_get_userbyid(d.defaclrole) as user, 
+	n.nspname as schema, 
+	case d.defaclobjtype 
+		when 'r' then 'tables' 
+		when 'f' then 'functions' 
+	end as object_type,
+	array_to_string(d.defaclacl, ' + ')  as default_privileges 
+from pg_catalog.pg_default_acl d 
+	left join pg_catalog.pg_namespace n 
+		on n.oid = d.defaclnamespace;
