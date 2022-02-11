45fa673b974392ab6dd6823062bc716a4d3d6835
Don Seiler <don@seiler.us>
2022-02-11 10:02:22 -0600

    Adding note about \ddp

--- postgresql/default_acl.sql
+++ postgresql/default_acl.sql
@@ -1,4 +1,5 @@
 -- From https://www.dba-ninja.com/2020/06/how-to-find-default-access-privileges-on-postgresql-with-pg_default_acl.html
+-- Can achieve same with \ddp in psql if just quick looking, but query will help build statements to revoke if needed
 select 
 	pg_get_userbyid(d.defaclrole) as user, 
 	n.nspname as schema, 
