21ecbac050a96280e5a5242a65842a57d6dd71c9
Don Seiler <don@seiler.us>
2023-03-01 16:33:02 -0600

    More meaningful standby lag check

--- postgresql/lag_standby.sql
+++ postgresql/lag_standby.sql
@@ -1,2 +1,5 @@
-select pg_is_in_recovery(),pg_is_wal_replay_paused(), pg_last_wal_receive_lsn(), pg_last_wal_replay_lsn(), 
-    pg_last_xact_replay_timestamp(), now(), age(now(), pg_last_xact_replay_timestamp());
+select pg_is_in_recovery(),pg_is_wal_replay_paused(),
+  pg_last_wal_receive_lsn(), pg_last_wal_replay_lsn(),
+  (pg_wal_lsn_diff(pg_last_wal_receive_lsn(), pg_last_wal_replay_lsn())/1024)::bigint as lsn_diff_kb,
+  pg_last_xact_replay_timestamp(), now(), age(now(), pg_last_xact_replay_timestamp()) as lag,
+  extract(epoch from now() - pg_last_xact_replay_timestamp())::int as lag_seconds;
