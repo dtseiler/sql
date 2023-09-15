7700ac5afdaa3c639bb2cae27b97c5ef171de930
Don Seiler <don@seiler.us>
2023-09-15 13:59:51 -0500

    Add FK list script

---
+++ postgresql/foreign_keys.sql
@@ -0,0 +1,8 @@
+-- from https://soft-builder.com/how-to-list-all-foreign-keys-in-postgresql-database/
+SELECT conrelid::regclass AS table_name, 
+       conname AS foreign_key, 
+       pg_get_constraintdef(oid) 
+FROM   pg_constraint 
+WHERE  contype = 'f' 
+AND    connamespace = 'public'::regnamespace   
+ORDER  BY conrelid::regclass::text, contype DESC;
