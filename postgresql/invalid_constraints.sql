9743677114e53ce5abea9b35f3d3135a0da80275
dtseiler <don@seiler.us>
2023-10-16 18:28:18 -0500

    contype key defs

--- postgresql/invalid_constraints.sql
+++ postgresql/invalid_constraints.sql
@@ -1,3 +1,4 @@
+-- c = check constraint, f = foreign key constraint, p = primary key constraint, u = unique constraint, t = constraint trigger, x = exclusion constraint
 select t.relname as table_name, c.conname as constraint_name, c.contype as constraint_type
 from pg_constraint c
         join pg_class t
