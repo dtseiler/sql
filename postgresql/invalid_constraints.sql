c13a0850c13dc24c3228c1f89e7f5d72813b6aee
dtseiler <don@seiler.us>
2023-10-16 18:42:04 -0500

    format

--- postgresql/invalid_constraints.sql
+++ postgresql/invalid_constraints.sql
@@ -1,6 +1,13 @@
--- c = check constraint, f = foreign key constraint, p = primary key constraint, u = unique constraint, t = constraint trigger, x = exclusion constraint
+/* 
+ * c = check constraint
+ * f = foreign key constraint
+ * p = primary key constraint
+ * u = unique constraint
+ * t = constraint trigger
+ * x = exclusion constraint
+ */
 select t.relname as table_name, c.conname as constraint_name, c.contype as constraint_type
 from pg_constraint c
-        join pg_class t
+    join pg_class t
         on c.conrelid=t.oid
 where c.convalidated = false;
